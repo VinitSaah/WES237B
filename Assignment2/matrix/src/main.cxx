cfb9a0a80a3816da7cd77e915a9bb22435865d59
Vinit Saah <vsaah@ucsd.edu>
2022-08-05 04:50:08 +0000

    Added Eigen and OpenCV matrices product and modified Makefile

--- Assignment2/matrix/src/main.cxx
+++ Assignment2/matrix/src/main.cxx
@@ -3,6 +3,9 @@
 #include <math.h>
 
 #include "mat.h"
+#include <eigen3/Eigen/Dense>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
 
 int main(int argc, const char* argv[])
 {
@@ -23,7 +26,7 @@ int main(int argc, const char* argv[])
     //m1.print_matrix();
     //m2.print_matrix();
     //Matrix<float>m3;
-    Matrix<float>m3 = m1*m2;
+    Matrix<float>m3 = m1*m2; // as per requirement.
     //m3.print_matrix();
 #if 0
     for(int i = 0; i < m3.get_matrix_height(); i++)
@@ -34,6 +37,7 @@ int main(int argc, const char* argv[])
         }
     }
 #endif
+#if 0
     /***Block size****/
     uint16_t block_size = 4; 
     Matrix<float>m4 = block_matrix_multiply(m1,m2,block_size);
@@ -50,5 +54,36 @@ int main(int argc, const char* argv[])
             }
         }
     }
+#endif
+    //Comparison with Eigen Matrix;
+    Eigen::MatrixXf eig_mat_a(row,col);
+    Eigen::MatrixXf eig_mat_b(row,col);
+
+    for(uint16_t i =0; i< row; i++)
+    {
+        for(uint16_t j=0;j< col; j++)
+        {
+            eig_mat_a(i,j) = i+1;
+            eig_mat_b(i,j) = i+1;
+        }
+    }
+
+    Eigen::MatrixXf eig_mat_c = eig_mat_a*eig_mat_b;
+    //std::cout << "Eigen Matrix\n" << eig_mat_c;
+
+    //Comparison with OpenCV
+    cv::Mat ocv_a = cv:: Mat::zeros(row, col, CV_32FC1); 
+    cv::Mat ocv_b = cv:: Mat::zeros(row, col, CV_32FC1);
+
+    for(uint16_t i =0; i< row; i++)
+    {
+        for(uint16_t j=0;j< col; j++)
+        {
+            ocv_a.at<float>(i,j) = i+1;
+            ocv_b.at<float>(i,j) = i+1;
+        }
+    }
+    cv::Mat ocv_c = ocv_a*ocv_b;
+    //std::cout << "Open CV Matrix\n" << ocv_c;
     return 0;
 }
