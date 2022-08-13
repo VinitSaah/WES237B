e2c9628f81c4a050907aa46473765a995c32a49b
Vinit Saah <vsaah@ucsd.edu>
2022-08-13 12:08:53 -0700

    Working commit of sobel unrolled

--- Assignment3/sobel/src/student_sobel.cpp
+++ Assignment3/sobel/src/student_sobel.cpp
@@ -55,7 +55,42 @@ void sobel(const Mat& src, Mat& dst)
 
 void sobel_unroll(const Mat& src, Mat& dst)
 {
-	// TODO
+// TODO
+	const int HEIGHT = src.rows;
+	const int WIDTH  = src.cols;
+
+	const uint8_t* src_ptr  = src.ptr<uint8_t>();
+    uint8_t* result_ptr = dst.ptr<uint8_t>();
+
+    //Gx
+	for(int i = 0; i < HEIGHT-2; i++)
+	{
+		int64_t reg3 = 0;
+		int64_t reg4 = 0;
+		for(int j = 0; j < WIDTH-2; j++)
+		{
+			int32_t reg1[3] = {0};
+			int32_t reg2[3] = {0};
+            for(int k = 0; k< 3; k++)
+			{
+				reg1[0] += src.at<uint8_t>((i+k),(j))   * sobel_kernel_x[k][0];
+				reg1[1] += src.at<uint8_t>((i+k),(j+1)) * sobel_kernel_x[k][1];
+				reg1[2] += src.at<uint8_t>((i+k),(j+2)) * sobel_kernel_x[k][2];
+
+				reg2[0] += src.at<uint8_t>((i+k),(j))   * sobel_kernel_y[k][0];
+				reg2[1] += src.at<uint8_t>((i+k),(j+1)) * sobel_kernel_y[k][1];
+				reg2[2] += src.at<uint8_t>((i+k),(j+2)) * sobel_kernel_y[k][2];
+			}
+			reg3 = reg1[0]+reg1[1]+reg1[2];
+			reg4 = reg2[0]+reg2[1]+reg2[2];
+			uint64_t gxgy= sqrt(reg3*reg3+reg4*reg4);
+			if(gxgy > 255)
+			{
+				gxgy = 255; //max value of uint8_t It corresponds to White
+			}
+            dst.at<uint8_t>(i,j) = (uint8_t)gxgy;  
+		}
+	}
 }
 
 void sobel_neon(const Mat& src, Mat& dst)
